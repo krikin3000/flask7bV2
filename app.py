102d716bc77b53113bb8f056673a5a7741369e2a
Domingo Fraga Hernandez <desarrolladorrapido@gmail.com>
2024-09-18 17:18:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,46 @@ from flask import request
 
 import pusher
 
+import mysql.connector
+import datetime
+import pytz
+
+con = mysql.connector.connect(
+  host="185.232.14.52",
+  database="u760464709_tst_sep",
+  user="u760464709_tst_sep_usr",
+  password="dJ0CIAFF="
+)
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    con.close()
     return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
+    con.close()
     return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
+    con.close()
     matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
+
+@app.route("/buscar")
+def buscar()
+    if not con.is_connected():
+        con.reconnect()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM sensor_log")
+    
+    registros = mycursor.fetchall()
+
+    return registros
 
 @app.route("/registrar", methods=["GET"])
 def registrar():
