c7655b337d26b14baf7ce318106906478e90427d
Domingo Fraga Hernandez <desarrolladorrapido@gmail.com>
2024-09-18 16:58:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def alumnosGuardar():
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
 
-@app.route("/registrar", methods=["GET"])
+@app.route("/registrar")
 def registrar():
     pusher_client = pusher.Pusher(
         app_id="1714541",
